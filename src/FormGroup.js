b1fb97733d49b48bc7b8aee9cbff7421376ec6d9
Dominic Woodman <dom_woodman@msn.com>
2019-03-16 17:00:01 +0000

    alter module

--- src/FormGroup.js
+++ src/FormGroup.js
@@ -1,17 +1,13 @@
-import React from 'react'
-import classnames from 'classnames'
+import React from "react";
+import classnames from "classnames";
 
 export default function FormGroup(props) {
   const className = {
-    'form-group': true,
-    'has-error': props.hasError
-  }
+    "form-group": true,
+    "is-invalid": props.hasError
+  };
   if (props.className) {
-    className[props.className] = true
+    className[props.className] = true;
   }
-  return (
-    <div className={classnames(className)}>
-      {props.children}
-    </div>
-  )
+  return <div className={classnames(className)}>{props.children}</div>;
 }
--- src/getError.js
+++ src/getError.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function getError({ hasError, error }) {
 	if (hasError && error) {
-		return <span className="help-block is-invalid">{error}</span>;
+		return <span className="invalid-feedback">{error}</span>;
 	}
 }
