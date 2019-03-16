f0e0089c786231f31d6697de51b4f825ff7f8df1
Dominic Woodman <dom_woodman@msn.com>
2019-03-16 16:52:11 +0000

    make changes for bootstrap 4

--- src/getError.js
+++ src/getError.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 
-export default function getError({hasError, error}) {
-  if (hasError && error) {
-    return <span className="help-block error-block">{error}</span>
-  }
+export default function getError({ hasError, error }) {
+	if (hasError && error) {
+		return <span className="help-block is-invalid">{error}</span>;
+	}
 }
