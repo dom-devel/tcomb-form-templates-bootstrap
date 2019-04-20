3ed725edf45ec96b528fad6277589b1c45ee7c2a
dom-devel <dom_woodman@msn.com>
2019-04-20 12:45:30 +0100

    push out changes to test switch to bulma

--- src/getError.js
+++ src/getError.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function getError({ hasError, error }) {
 	if (hasError && error) {
-		return <span className="invalid-feedback">{error}</span>;
+		return <span className="help is-danger">{error}</span>;
 	}
 }
--- src/renderFormGroup.js
+++ src/renderFormGroup.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import FormGroup from './FormGroup'
+import React from "react";
+import FormGroup from "./FormGroup";
 
 export default function renderFormGroup(children, { path, hasError }) {
-  let className = `form-group-depth-${path.length}`
-  if (path.length > 0) {
-    className += ` form-group-${path.join('-')}`
-  }
-  return React.createElement.apply(null, [FormGroup, { className, hasError }].concat(children))
+	let className = `form-group-depth-${path.length}`;
+	if (path.length > 0) {
+		className += ` field form-group-${path.join("-")}`;
+	}
+	return React.createElement.apply(
+		null,
+		[FormGroup, { className, hasError }].concat(children)
+	);
 }
