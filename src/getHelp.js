f1444bf53910f2280dec6fcc1befd6ca4ba18a76
dom-devel <dom_woodman@msn.com>
2020-01-09 19:51:21 +0000

    add help class to bulma template

--- src/getHelp.js
+++ src/getHelp.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
 
-export default function getHelp({help, attrs}) {
-  if (help) {
-    return <span className="help-block" id={`${attrs.id}-tip`}>{help}</span>
-  }
+export default function getHelp({ help, attrs }) {
+	if (help) {
+		return (
+			<span className="help help-block" id={`${attrs.id}-tip`}>
+				{help}
+			</span>
+		);
+	}
 }
